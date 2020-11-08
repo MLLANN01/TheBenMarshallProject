66889c3c8b1d9336b3a2cf7beeb17b0b4978912f
Marshall Lanning <35392661+MLLANN01@users.noreply.github.com>
2020-11-07 23:29:37 -0500

    Added API

---
+++ ServerHelperApi/src/main/java/com/serverHelper/dao/ServerHelperApiApplication.java
@@ -0,0 +1,13 @@
+package com.serverHelper.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerHelperApiApplication.class, args);
+	}
+
+}
---
+++ ServerHelperApi/src/test/java/com/serverHelper/dao/ServerHelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverHelper.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerHelperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
